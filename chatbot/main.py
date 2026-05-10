461ce3f5244bd697e19b5a8bd8acc84d633f7677
Qasim <mianqasim065@gmail.com>
2026-05-10 19:10:21 +0500

    feat(langchain messages) Solve short term memory using langchain messages

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,18 +1,27 @@
 from langchain_mistralai import ChatMistralAI
+from langchain.messages import HumanMessage, AIMessage, SystemMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model = ChatMistralAI(model="mistral-medium-3-5")
+
 print("============ Welcome ==================")
 print("Press 0 to exit.")
-messages = []
+
+persona = input("What persona do you want? e.g funny AI, Angry Teacher etc")
+
+messages = [
+    SystemMessage(content=f"You are a {persona}"),
+]
 
 while True:
     prompt = input("You: ")
-    messages.append(prompt)
+    messages.append(HumanMessage(content=prompt))
     if prompt == "0":
         break
     res = model.invoke(messages)
-    messages.append(res.content)
+    messages.append(AIMessage(content=res.content))
     print("Bot", res.content)
+
+print(messages)
