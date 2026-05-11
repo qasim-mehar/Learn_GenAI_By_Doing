8c51afbf9d667da34424e9c52131753822832b28
Qasim <mianqasim065@gmail.com>
2026-05-11 20:31:27 +0500

    feat(schema): implement a pydantic output schema

--- structured_output/main.py
+++ structured_output/main.py
@@ -1,8 +1,22 @@
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel
+from typing import List, Optional
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+class movieSummarySchema(BaseModel):
+    title: str
+    release_year: str
+    director: str
+    genre: List[str]
+    cast: List[str]
+    premise: str
+    IMDb_rating: int
+    music_composer: str
+
 
 model = ChatMistralAI(
     model="mistral-medium-3-5",
