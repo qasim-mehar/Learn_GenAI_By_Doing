be267b5520f9e3691057fe4febe57c4e5163b998
Qasim <mianqasim065@gmail.com>
2026-05-14 18:49:56 +0500

    feat(chunking): implement character base chunking

---
+++ prepPal_RAG/chunking/characterTextSplitter.py
@@ -0,0 +1,18 @@
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+
+data = TextLoader("../temp.txt")
+docs = data.load()
+
+
+splitter = CharacterTextSplitter(
+    separator=" ",
+    chunk_size=10,
+    chunk_overlap=1,
+)
+chunks = splitter.create_documents([docs[0].page_content])
+
+
+for i in chunks:
+    print(i.page_content)
+    print()
