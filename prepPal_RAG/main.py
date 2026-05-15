3c38203df5764c5bfe33e75f4713a53e6bac3fc4
Qasim <mianqasim065@gmail.com>
2026-05-15 21:20:51 +0500

    feat(embedding): implement embedding in prepPal project

--- prepPal_RAG/main.py
+++ prepPal_RAG/main.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
+from langchain_mistralai import MistralAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
 
 load_dotenv()
 
@@ -19,6 +22,8 @@ splitter = RecursiveCharacterTextSplitter(
     chunk_overlap=50,
 )
 chunks = splitter.create_documents(docs)
+
+embedding = MistralAIEmbeddings(model="mistral-embed")
 
 
 prompt_template = ChatPromptTemplate.from_messages(
