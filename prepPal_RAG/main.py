b9f9ae6af051de8dd47a79c00a7dac959c32e326
Qasim <mianqasim065@gmail.com>
2026-05-16 15:55:53 +0500

    feat(rag): implement continuous RAG query loop for PDF chat

--- prepPal_RAG/main.py
+++ prepPal_RAG/main.py
@@ -3,8 +3,7 @@ from langchain_mistralai import ChatMistralAI
 from langchain_mistralai import MistralAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 
 
 load_dotenv()
@@ -13,31 +12,45 @@ model = ChatMistralAI(
     model="mistral-medium-3-5",
 )
 
-data = PyPDFLoader("doc_loader/lec 1.pdf")
+data = PyPDFLoader("lec 1.pdf")
 docs = data.load()
 
+embedding = MistralAIEmbeddings()
 
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=50,
+vectorstore = Chroma(
+    persist_directory="chroma_db",
+    embedding_function=embedding,
 )
-chunks = splitter.create_documents(docs)
 
-embedding = MistralAIEmbeddings(model="mistral-embed")
-
-
+retriever = vectorstore.as_retriever(
+    search_type="mmr",
+    kwargs={
+        "k": 4,
+        "fetch_k": 10,
+    },
+)
 prompt_template = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a great text summarizer with 15 years of experince now, you have to summarize the text",
+            "You are a great AI assistant with 15 years of experience. Answer the user query using ONLY the given context. If the answer isn't available in the given context, just answer 'I couldn't find the answer'.\n\nContext:\n{context}",
         ),
-        ("human", "{data}"),
+        ("human", "{query}"),
     ]
 )
 
-final_prompt = prompt_template.format_messages(data=docs)
+print("-------------RAG-----------------")
+print("Press 0 to exit.")
 
-res = model.invoke(final_prompt)
 
-print(res.content)
+while True:
+    query = input("You: ")
+    if query == "0":
+        break
+    docs = retriever.invoke(query)
+
+    context = "/n/n".join([doc.page_content for doc in docs])
+
+    final_prompt = prompt_template.format_messages(context=context, query=query)
+    res = model.invoke(final_prompt)
+    print(f"\n AI: {res.content}")
