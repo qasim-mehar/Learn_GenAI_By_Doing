17e232c2c3df56144731cbdaca2f6db15b2e9b57
Qasim <mianqasim065@gmail.com>
2026-05-23 13:32:36 +0500

    feat(agent): implement llm tool-binding and invocation logic

--- tools/toolcalling.py
+++ tools/toolcalling.py
@@ -21,3 +21,20 @@ def get_word_count(text: str) -> int:
 
 tools_list = [get_text_length, get_word_count]
 tools_dict = {t.name: t for t in tools_list}
+
+llm = ChatMistralAI(model="mistral-medium-3-5")
+
+llm_with_tool = llm.bind_tools(tools_list, tool_choice="any")
+
+messages = [
+    SystemMessage(
+        content="You are a helpful text analysis assistant. Even if the user just types a single word, use your tools to analyze it and clearly tell them the length."
+    )
+]
+
+prompt = input("You: ")
+query = HumanMessage(prompt)
+messages.append(query)
+
+llm_result = llm_with_tool.invoke(messages)
+messages.append(llm_result)
